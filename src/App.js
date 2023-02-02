8eb8b46fa3c1bb14524447a53cdc7cd22ee9ce01
rwony <dev.rwlee@gmail.com>
2023-02-02 23:44:54 +0900

    Route 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import MyHeader from './components/MyHeader'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Todo from './pages/Todo'
+
+import './App.css'
+
+export const AuthContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyHeader />
+
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
