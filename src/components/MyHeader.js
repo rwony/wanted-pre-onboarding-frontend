b7d62b280d0046d5f9b6071ff5324b41b3881d46
rwony <dev.rwlee@gmail.com>
2023-02-03 19:01:05 +0900

    헤더 작성

---
+++ src/components/MyHeader.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import styled from 'styled-components'
+
+const MyHeader = () => {
+  const token = window.localStorage.getItem('userToken')
+  const navigate = useNavigate()
+
+  const Logout = () => {
+    window.localStorage.removeItem('userToken')
+    navigate('/', { replace: true })
+  }
+
+  return (
+    <Header>
+      <h1>2023 원티드 프리온보딩 프론트엔드 - 선발 과제</h1>
+
+      <nav>
+        <ul>
+          {!token ? (
+            <>
+              <li>
+                <Link to={'signin'}>로그인</Link>
+              </li>
+              <li>
+                <Link to={'signup'}>회원가입</Link>
+              </li>
+            </>
+          ) : (
+            <li>
+              <button type="button" onClick={Logout}>
+                로그아웃
+              </button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </Header>
+  )
+}
+
+const Header = styled.header`
+  background-color: #fff;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.08);
+
+  h1 {
+    padding: 8px;
+    font-size: 24px;
+    text-align: center;
+  }
+
+  ul {
+    display: flex;
+    justify-content: end;
+
+    li {
+      padding: 20px 0;
+
+      button,
+      a {
+        font-size: 14px;
+        font-weight: 700;
+
+        &:hover {
+          opacity: 0.7;
+          transition: opacity 0.2s ease-in-out;
+        }
+      }
+
+      &::after {
+        content: '';
+        margin-right: 16px;
+      }
+    }
+  }
+`
+
+export default MyHeader
